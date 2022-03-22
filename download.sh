c56d66376e705904b7b4d1013bb88cbb2870dccb
ASiCo1116 <b05611046@ntu.edu.tw>
2022-03-22 22:17:35 +0800

    [0322] modified cache

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
 wget https://www.dropbox.com/s/0s6ggpu2ai16kaw/best_slot.ckpt?dl=1 -O best_slot.ckpt
 wget https://www.dropbox.com/s/x8tckdfcz52hyho/best_intent.ckpt?dl=1 -O best_intent.ckpt
-wget https://www.dropbox.com/sh/j8q7bw5zt7jy5ft/AAAqXUYz_LiQSHEcwKyM_kqva?dl=1
+wget https://www.dropbox.com/s/mgqpkqbdr7gvqmv/cache.zip?dl=1 -O cache.zip
+unzip cache.zip
